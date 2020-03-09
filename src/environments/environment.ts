dc2411b87cb2a2f58d18866395dc568f7a69dd34
pquiroza <pedro.quiroz@gmail.com>
2020-03-09 17:24:08 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-  apiKey: "AIzaSyAeE1UZ4_hxQpG9M85iT9HFQDuuJPnfFv4",
- authDomain: "ligueros-af5b3.firebaseapp.com",
- databaseURL: "https://ligueros-af5b3.firebaseio.com",
- projectId: "ligueros-af5b3",
- storageBucket: "ligueros-af5b3.appspot.com",
- messagingSenderId: "290557695821",
- appId: "1:290557695821:web:62d5f7e53cc626b0648904",
- measurementId: "G-VFK6CNBKME"
+
 }
 };
 
